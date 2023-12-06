8f0687215e78302449451896ef7ab77c0b4e5a28
Miguel Dias <miguel.dias@estudante.ufjf.br>
2023-12-06 20:32:22 -0300

    Botão criado bugs arrumados

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 document.entrada.addEventListener('submit',leformulario)
-leformulario()
 function leformulario(event) {
     event.preventDefault()
     const fruta = document.entrada.fruta.value
-    const quantidade = document.entrada.fruta.value
+    const quantidade = document.entrada.quantidade.value
     console.log(fruta,quantidade)
 
 }
