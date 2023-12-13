e126d589e2b8c6e41b1585643f95c7ac8a33db6c
Miguel Dias <miguel.dias@estudante.ufjf.br>
2023-12-13 19:35:24 -0300

    html arrumad0

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 document.entrada.addEventListener('submit',leformulario)
+
+const olJoao = document.querySelector("#olJoao")
+const olMaria = document.querySelector("#olMaria")
+
 function leformulario(event) {
     event.preventDefault()
     const fruta = document.entrada.fruta.value
     const quantidade = document.entrada.quantidade.value
     console.log(fruta,quantidade)
 
+    let origem = document.entrada.origem.value
+    let destino = document.entrada.destino.value
+    console.log(`Solicitado ${origem} doa ${quantidade} ${fruta} para ${destino}`)
 }
